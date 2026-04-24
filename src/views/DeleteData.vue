a325809d1099c44bab4d01caf7e809f4313ff068
Dilip Reddymalla <reddymaladilip@gmail.com>
2026-04-24 19:50:43 +0530

    implement employee CRUD with Vue Router and Axios

---
+++ src/views/DeleteData.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <h2>This page is to delte employee data</h2>
+        <DeleteComp />
+
+    </div>
+</template>
+
+<script>
+import DeleteComp from '@/components/DeleteComp.vue';
+export default{
+    name:"DeleteData",
+    components:{
+        DeleteComp,
+    }
+}
+</script>
---
+++ src/views/GetData.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <h1>This page shows the employee data in formated table</h1>
+        <GetComp />
+    </div>
+</template>
+
+<script>
+import GetComp from '@/components/GetComp.vue';
+export default{
+    name:"GetData",
+    components:{
+        GetComp,
+    }
+}
+
+</script>
